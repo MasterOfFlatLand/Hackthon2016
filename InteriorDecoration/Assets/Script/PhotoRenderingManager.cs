35b5f8a2667b239162df23794a8fbb13321cd1ed
Yang Ruijian <ruijian.yang@gmail.com>
2016-11-02 22:10:19 +0800

    add Unity project;

---
+++ InteriorDecoration/Assets/Script/PhotoRenderingManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Text;
+
+public class PhotoRenderingManager : MonoBehaviour {
+    public string serverUrl;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetKeyDown(KeyCode.R)) {
+            //Camera cmr = Camera.main;
+            string s = "this is a test!";
+            StartCoroutine(RequestRendering(Encoding.ASCII.GetBytes(s)));
+        }
+	}
+
+    IEnumerator RequestRendering(byte[] parameters) {
+        WWW request = new WWW(serverUrl, parameters);
+
+        yield return request;
+
+        Debug.Log("return information from server: " + request.text);
+    }
+}
