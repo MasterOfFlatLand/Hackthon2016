2e01fb8ecd2cd2e00f09b79b4e25a1ce70f682c4
YangAlbert <ruijian.yang@gmail.com>
2016-11-25 12:07:32 +0800

    1. add loading scene; 2. add progress store; 3. VR UI tested;

---
+++ InteriorDecoration/Assets/Script/progressTrigger.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class progressTrigger : MonoBehaviour {
+    public float loadingLength = 10;
+
+    private float startTime;
+    private float progressLength;
+    private RectTransform trans;
+	// Use this for initialization
+	void Start () {
+        startTime = Time.time;
+
+        trans = transform as RectTransform;
+        progressLength = trans.rect.width;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float deltaTime = Time.time - startTime;
+        float tarWidth = (1 - deltaTime / loadingLength) * progressLength;
+
+        if (tarWidth <= 0)
+        {
+            SceneManager.LoadScene("BigBang");
+        }
+        else
+        {
+            trans.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, tarWidth);
+        }
+	}
+}
